80f7d92bc14c8e36c4762514823de0ef795f76c9
Levy Candido <levyjunior@plusoft.com.br>
2019-07-21 22:26:51 -0300

    Google Auth

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 const express = require ('express');
-const passport = require ('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const mongoose = require ('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require ('./config/keys');
+require('./models/User');
+require ('./services/passport');
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-passport.use(
-  new GoogleStrategy(
-  {
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClienteSecret,
-    callbackURL: '/auth/google/callback'
-  },
-  (accessToken, refreshToken, profile, done) => {
-    console.log('accessToken', accessToken );
-    console.log('accessToken', refreshToken );
-    console.log('accessToken', profile );
-  }
- )
-);
-
-app.get(
-  '/auth/google',
-  passport.authenticate('google', {
-    scope: ['profile', 'email']
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys:[keys.cookieKey]
   })
 );
 
-app.get('/auth/google/callback', passport.authenticate('google'));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
